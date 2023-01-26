9533504c75b926f80f9d19b999aa0cc78f60f682
lucasdarini <lucas.darini@inovafarma.com.br>
2023-01-26 11:14:38 -0300

    feat(LoginView): add new page view

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todos_mobx/components/avatar_widget.dart';
 import 'package:todos_mobx/components/button_widget.dart';
 import 'package:todos_mobx/themes/app_theme.dart';
+import 'package:todos_mobx/views/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,36 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ToDos',
       theme: appTheme(),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        child: Column(
-          children: <Widget>[
-            const SizedBox(
-              height: 40,
-            ),
-            CustomButton(
-              text: 'Teste',
-              callback: () {},
-              width: 300,
-              image: 'assets/images/google.png',
-            ),
-            const CustomAvatar(
-              path: 'https://place-hold.it/200',
-              width: 120,
-            ),
-          ],
-        ),
-      ),
+      home: const LoginView(),
     );
   }
 }
