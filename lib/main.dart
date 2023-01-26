246f9b7a53494112cd90cc62e302dd09fb1a3f54
lucasdarini <lucas.darini@inovafarma.com.br>
2023-01-26 10:14:27 -0300

    feat: add new page HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todos_mobx/components/button_widget.dart';
 import 'package:todos_mobx/themes/app_theme.dart';
 
 void main() {
@@ -13,7 +14,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ToDos',
       theme: appTheme(),
-      home: null,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        child: CustomButton(
+          text: 'Teste',
+          callback: () {},
+          width: 300,
+          image: 'assets/images/google.png',
+        ),
+      ),
     );
   }
 }
