ed7caca3c4daa830cad015105c176cb342a7e9a0
lucasdarini <lucas.darini@inovafarma.com.br>
2023-02-08 11:55:45 -0300

    refactor(Navbar): buttons state change

--- lib/widgets/navbar_widget.dart
+++ lib/widgets/navbar_widget.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+import 'package:todos_mobx/controllers/todo_controller.dart';
+import 'package:todos_mobx/stores/app_store.dart';
 
 class Navbar extends StatelessWidget {
   const Navbar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<AppStore>(context);
+    final controller = TodoController(store);
+
     return SizedBox(
       width: double.infinity,
       height: 80,
@@ -12,35 +19,62 @@ class Navbar extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          TextButton(
-            child: const Text(
-              'Hoje',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+          Observer(
+            builder: (_) => TextButton(
+              child: Text(
+                'Hoje',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: store.currentState == 'today'
+                      ? FontWeight.bold
+                      : FontWeight.w100,
+                  color: store.currentState == 'today'
+                      ? Theme.of(context).primaryColor
+                      : Colors.black,
+                ),
               ),
+              onPressed: () {
+                controller.changeSelection('today');
+              },
             ),
-            onPressed: () {},
           ),
-          TextButton(
-            child: const Text(
-              'Amanhã',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w100,
+          Observer(
+            builder: (_) => TextButton(
+              child: Text(
+                'Amanhã',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: store.currentState == 'tomorrow'
+                      ? FontWeight.bold
+                      : FontWeight.w100,
+                  color: store.currentState == 'tomorrow'
+                      ? Theme.of(context).primaryColor
+                      : Colors.black,
+                ),
               ),
+              onPressed: () {
+                controller.changeSelection('tomorrow');
+              },
             ),
-            onPressed: () {},
           ),
-          TextButton(
-            child: const Text(
-              'Todas',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w100,
+          Observer(
+            builder: (_) => TextButton(
+              child: Text(
+                'Todas',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: store.currentState == 'all'
+                      ? FontWeight.bold
+                      : FontWeight.w100,
+                  color: store.currentState == 'all'
+                      ? Theme.of(context).primaryColor
+                      : Colors.black,
+                ),
               ),
+              onPressed: () {
+                controller.changeSelection('all');
+              },
             ),
-            onPressed: () {},
           ),
         ],
       ),
